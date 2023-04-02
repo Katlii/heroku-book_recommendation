b07fb148189d51a8e7797f9b9f9097a01885e577
Katsiaryna Zubaryk <zubaryk.katsiaryna@gmail.com>
2023-04-02 21:34:50 +0200

    Update book_recommender.py

--- book_recommender.py
+++ book_recommender.py
@@ -27,7 +27,9 @@ def get_books():
     return jsonify({'books': books})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
     
 
