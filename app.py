7a91503d97c0df854fb223ba02237130ac26d691
Katsiaryna Zubaryk <zubaryk.katsiaryna@gmail.com>
2023-04-04 23:04:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 class functions:
-    def Correlation(self, ratings, books, book_title):
+    def Correlation(self, ratings, books, book_title, dataset_lowercase):
         author_name = dataset_lowercase.loc[dataset_lowercase['title'] == book_title, 'author'].unique()
         author_readers = dataset_lowercase['user_id'][(dataset_lowercase['title']==book_title) & (dataset_lowercase['author'].str.contains(author_name[0]))]
         author_readers = author_readers.tolist()
@@ -35,10 +35,25 @@ def recommend_books():
     data = request.get_json()
     book_title = data.get('book_title')
     ratings = pd.read_csv('BX-Book-Ratings.csv', sep=';', encoding='cp1251', error_bad_lines=False)
-    # more code to preprocess ratings and books
+    ratings = ratings[ratings['Book-Rating']!=0]
+
+    # load books
+    books = pd.read_csv('BX-Books.csv',  encoding='cp1251', sep=';', error_bad_lines=False)  #Use on_bad_lines in the future.
+
+    ratings=ratings.rename(columns={'User-ID':'user_id', 'Book-Rating': 'ratings'})
+    books= books.rename(columns={'Book-Title':'title', 'Book-Author': 'author', 'Year-Of-Publication': 'year', 'Publisher':'publisher'})
+
+    isbnForUser=ratings.groupby('user_id').ISBN.nunique()
+    average=sum(isbnForUser.to_dict().values())/len(isbnForUser)
+    x=isbnForUser >5
+    x=x[x].index.tolist()
+    ratings=ratings[ratings['user_id'].isin(x)]
+
+    dataset = pd.merge(ratings, books, on=['ISBN'])
+    dataset_lowercase=dataset.apply(lambda x: x.str.lower() if(x.dtype == 'object') else x)
     dataset_lowercase= dataset_lowercase.drop_duplicates(['user_id', 'title'])
     dataset_lowercase=dataset_lowercase.drop(['Image-URL-S', 'Image-URL-M', 'Image-URL-L'], axis=1)
-    dataset_for_corr, ratings_data_raw = functions().Correlation(ratings, books, book_title)
+    dataset_for_corr, ratings_data_raw = functions().Correlation(ratings, books, book_title, dataset_lowercase)
     result, worst = functions().Book_recomendation(book_title, dataset_for_corr, ratings_data_raw)
     response = {'top_recommendations': result.to_dict(), 'worst_recommendations': worst.to_dict()}
     return jsonify(response)
